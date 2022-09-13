355e72472ee32b36b30240761fcd9da0930ebe74
ElsayedTork <elsayed.torky@inovola.com>
2022-09-13 11:54:11 +0200

    handle movement and go up an fall

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <div v-for="step in steps" :key="step.id">
         <div
           class="twor__step"
-          :style="{ top: step.top + 'px', left: step.left + 'px' }"
+          :style="{ bottom: step.bottom + 'px', left: step.left + 'px' }"
         ></div>
       </div>
       <div class="twor__player">
@@ -17,7 +17,7 @@
           ref="div"
           tabindex="0"
           :class="jumb == true ? `jumb-${this.dir}` : ''"
-          :style="{ bottom: this.imgx + '%', left: this.imgy + '%' }"
+          :style="{ bottom: this.imgx + 'px', left: this.imgy + 'px' }"
         />
       </div>
     </section>
@@ -35,27 +35,41 @@ export default {
       jumb: false,
       count: 0,
       dir: '',
+      stepNumber: 0,
     };
   },
   methods: {
     handleRigth() {
-      this.imgy += 2;
+      this.imgy += 25;
       this.varr = !this.varr;
       this.dir = 'r';
       this.jumb = true;
     },
     handleLeft() {
       this.dir = 'l';
-      this.imgy -= 2;
+      this.imgy -= 25;
       this.varr = !this.varr;
       this.jumb = true;
     },
     handleUp() {
-      this.imgx += 25;
+      this.imgx += 180;
       this.dir = 'up';
       this.jumb = true;
       this.varr = !this.varr;
-      setTimeout(() => (this.imgx = 0), 500);
+      //console.log('this.steps[4].top &&', this.steps[0].bottom);
+      //console.log('this.imgx', this.imgx);
+      setTimeout(() => {
+        if (
+          this.imgx > this.steps[this.stepNumber].bottom &&
+          this.imgy + 60 > this.steps[this.stepNumber].left &&
+          this.imgy < this.steps[this.stepNumber].left + 300
+        ) {
+          this.imgx = this.steps[this.stepNumber].bottom + 20;
+          this.stepNumber++;
+        } else {
+          this.imgx = 0;
+        }
+      }, 200);
     },
     randomNumbwr(to) {
       return Math.floor(Math.random() * to);
@@ -66,33 +80,33 @@ export default {
       this.count -= 220;
       this.steps.push({
         id: this.randomNumbwr(1000),
-        top: this.count,
+        bottom: this.count,
         left: this.randomNumbwr(500),
       });
     }
     this.steps = [
-      { id: 1, top: 50, left: 220 },
-      { id: 2, top: 150, left: 150 },
-      { id: 3, top: 250, left: 600 },
-      { id: 4, top: 350, left: 870 },
-      { id: 5, top: 450, left: 400 },
-      { id: 6, top: -50, left: 220 },
-      { id: 7, top: -150, left: 150 },
-      { id: 8, top: -250, left: 600 },
-      { id: 9, top: -350, left: 870 },
-      { id: 10, top: -450, left: 400 },
+      { id: 1, bottom: 100, left: 200 },
+      { id: 2, bottom: 200, left: 350 },
+      { id: 3, bottom: 300, left: 600 },
+      { id: 4, bottom: 400, left: 870 },
+      { id: 5, bottom: 500, left: 400 },
+      { id: 6, bottom: 600, left: 220 },
+      { id: 7, bottom: 700, left: 150 },
+      { id: 8, bottom: 800, left: 600 },
+      { id: 9, bottom: 900, left: 870 },
+      { id: 10, bottom: 1000, left: 400 },
     ];
   },
   mounted() {
     this.$refs.div.focus();
-    setTimeout(() => {
-      setInterval(() => {
-        this.steps = this.steps.map((step) => ({
-          ...step,
-          top: step.top + 5,
-        }));
-      }, 200);
-    }, 20);
+    //   setTimeout(() => {
+    //     setInterval(() => {
+    //       this.steps = this.steps.map((step) => ({
+    //         ...step,
+    //         top: step.top + 5,
+    //       }));
+    //     }, 200);
+    //   }, 20);
   },
   components: {},
 };
@@ -110,7 +124,7 @@ html {
   background-color: #000;
   position: relative;
   &__step {
-    width: 350px;
+    width: 300px;
     background-color: orange;
     height: 20px;
     border-radius: 10px 10px 0 0;
@@ -120,6 +134,7 @@ html {
   &__player {
     img {
       height: 100px;
+      width: 60px;
       position: absolute;
       transition: rotate 0.7s ease-in-out;
     }
