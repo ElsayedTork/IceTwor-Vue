e3f9c9693cfd1b38a93d9db3215aa8a6fa908d56
ElsayedTork <elsayed.torky@inovola.com>
2022-09-14 02:02:19 +0200

    handle lose if imgx <= 0

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,32 @@
 <template>
-  <div class="container">
-    <section class="twor" v-if="lose == false && stepNumber < 4">
-      <div v-for="step in steps" :key="step.id">
-        <div
-          class="twor__step"
-          :style="{ bottom: step.bottom + 'px', left: step.left + 'px' }"
-        ></div>
-      </div>
-      <div class="twor__player">
-        <img
-          src="./assets/Mario.png"
-          alt=""
-          @keyup.right="handleRigth"
-          @keyup.left="handleLeft"
-          @keyup.up="handleUp"
-          ref="div"
-          tabindex="0"
-          :class="jumb == true ? `jumb-${this.dir}` : ''"
-          :style="{ bottom: this.imgx + 'px', left: this.imgy + 'px' }"
-        />
-      </div>
-    </section>
-    <section class="tower-lose" v-else>
-      <h2>{{ messageEndGame }}</h2>
-    </section>
-    <h2 class="score">Your Score Is : {{ stepNumber }}</h2>
+  <div class="twor">
+    <div class="container-fluid">
+      <section class="twor__game" v-if="lose == false && stepNumber < 15">
+        <div v-for="step in steps" :key="step.id">
+          <div
+            class="twor__game__step"
+            :style="{ bottom: step.bottom + 'px', left: step.left + 'px' }"
+          ></div>
+        </div>
+        <div class="twor__game__player">
+          <img
+            src="./assets/Mario.png"
+            alt=""
+            @keyup.right="handleRigth"
+            @keyup.left="handleLeft"
+            @keyup.up="handleUp"
+            ref="div"
+            tabindex="0"
+            :class="jumb == true ? `jumb-${this.dir}` : ''"
+            :style="{ bottom: this.imgx + 'px', left: this.imgy + 'px' }"
+          />
+        </div>
+      </section>
+      <section class="twor__lose" v-else>
+        <h2>{{ messageEndGame }}</h2>
+      </section>
+      <h2 class="twor__score">Your Score Is : {{ stepNumber }}</h2>
+    </div>
   </div>
 </template>
 
@@ -32,7 +34,23 @@
 export default {
   data() {
     return {
-      steps: [],
+      steps: [
+        { id: 1, bottom: 100, left: 200 },
+        { id: 2, bottom: 200, left: 350 },
+        { id: 3, bottom: 300, left: 600 },
+        { id: 4, bottom: 400, left: 500 },
+        { id: 5, bottom: 500, left: 400 },
+        { id: 6, bottom: 600, left: 320 },
+        { id: 7, bottom: 700, left: 250 },
+        { id: 8, bottom: 800, left: 150 },
+        { id: 9, bottom: 900, left: 200 },
+        { id: 10, bottom: 1000, left: 300 },
+        { id: 11, bottom: 1100, left: 200 },
+        { id: 12, bottom: 1200, left: 100 },
+        { id: 13, bottom: 1300, left: 150 },
+        { id: 14, bottom: 1400, left: 250 },
+        { id: 15, bottom: 1500, left: 350 },
+      ],
       varr: false,
       imgx: 0,
       imgy: 0,
@@ -47,7 +65,7 @@ export default {
     messageEndGame() {
       if (this.lose == true) {
         return 'You Lose The Game';
-      } else if (this.stepNumber >= 4) {
+      } else if (this.stepNumber >= 15) {
         return 'You Win The Game';
       } else {
         return '';
@@ -119,27 +137,6 @@ export default {
       }
     },
   },
-  created() {
-    for (let x = 0; x <= 20; x++) {
-      this.steps.push({
-        id: this.randomNumbwr(1000),
-        bottom: this.count,
-        left: this.randomNumbwr(500),
-      });
-    }
-    this.steps = [
-      { id: 1, bottom: 100, left: 200 },
-      { id: 2, bottom: 200, left: 350 },
-      { id: 3, bottom: 300, left: 600 },
-      { id: 4, bottom: 400, left: 500 },
-      { id: 5, bottom: 500, left: 400 },
-      { id: 6, bottom: 600, left: 220 },
-      { id: 7, bottom: 700, left: 150 },
-      { id: 8, bottom: 800, left: 600 },
-      { id: 9, bottom: 900, left: 870 },
-      { id: 10, bottom: 1000, left: 400 },
-    ];
-  },
   mounted() {
     this.$refs.div.focus();
     setTimeout(() => {
@@ -150,6 +147,9 @@ export default {
             bottom: step.bottom - 5,
           }));
           this.imgx -= 5;
+          if (this.imgx <= 0) {
+            this.lose = true;
+          }
         }, 200);
       }, 20);
     }, 2000);
@@ -157,56 +157,53 @@ export default {
 };
 </script>
 
-<style lang="scss" class="scoped">
-@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
-
-html {
-  font-family: 'Roboto', sans-serif;
-}
+<style lang="scss" scoped>
 .twor {
-  width: 100%;
-  height: 95vh;
-  background-color: #000;
-  position: relative;
-  &__step {
-    width: 300px;
-    background-color: orange;
-    height: 20px;
-    border-radius: 10px 10px 0 0;
-    border-color: #fff;
-    position: absolute;
-  }
-  &__player {
-    img {
-      height: 100px;
-      width: 60px;
+  &__game {
+    width: 100%;
+    height: 100vh;
+    background-color: #000;
+    position: relative;
+    &__step {
+      width: 300px;
+      background-color: orange;
+      height: 20px;
+      border-radius: 10px 10px 0 0;
+      border-color: #fff;
       position: absolute;
-      transition: rotate 0.7s ease-in-out;
+    }
+    &__player {
+      img {
+        height: 100px;
+        width: 60px;
+        position: absolute;
+        transition: rotate 0.7s ease-in-out;
+      }
+    }
+    .jumb-r {
+      rotate: y 50deg;
+    }
+    .jumb-l {
+      rotate: y -50deg;
+    }
+
+    .jumb-up {
+      rotate: z 360deg;
     }
   }
-  .jumb-r {
-    rotate: y 50deg;
+  &__lose {
+    background-color: #000;
+    color: orange;
+    padding: 30px 100px;
+    font-size: 45px;
+    text-align: center;
   }
-  .jumb-l {
-    rotate: y -50deg;
+  &__score {
+    position: absolute;
+    top: 10px;
+    right: 200px;
+    color: #fff;
+    font-size: 20px;
   }
-
-  .jumb-up {
-    rotate: z 360deg;
-  }
-}
-.tower-lose {
-  background-color: #000;
-  color: orange;
-  padding: 30px 100px;
-  font-size: 45px;
-  text-align: center;
-}
-.score {
-  position: absolute;
-  top: 10px;
-  right: 200px;
-  color: #fff;
-  font-size: 20px;
 }
 </style>
